71496eff88f0e3827bba9bf0310299f3e9b865b6
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-02-28 14:25:42 +0300

    practised selecting elements using jquery

--- Lesson2-Selecting-Elements/index.js
+++ Lesson2-Selecting-Elements/index.js
@@ -1,3 +1,13 @@
-$('div').on('click', ()=>{
-   $('div').html('Değiştirilmiş'); 
-})
+const el1 = $('div');
+console.log(el1)
+
+const el2 = $('.div2')
+console.log(el2)
+
+const el3 = $('#div3')
+console.log(el3)
+
+const el4 = $('#div3, .div2');
+console.log(el4)
+
+console.log($(document).ready());
