125ee0796ca9136d239a313b9e95464e9f3b34ad
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-03-04 18:17:22 +0300

    feat: basic element animations tested

---
+++ Lesson3-Element-Effects/index.js
@@ -0,0 +1,25 @@
+console.log($('div').first());
+const val = '<h1>Berk Akın</h1>';
+
+$('div').first().on('click',()=>{
+    $('div').first().html(val)
+});
+
+$('div').click((e)=>{
+    $(e.target).text('Clickedd')
+    $(e.target).hide()
+})
+
+
+function clicker(e) {
+    console.log(e)
+}
+
+$('#gizle').click(()=>{
+    $('div').hide('slow')
+
+})
+
+$('#goster').click(()=>{
+    $('div').show(10000)
+})
