10d2589df9bece3d24e72b10bfa6a26395730e95
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-03-13 19:42:05 +0300

    practised element addition features

--- Lesson6-Element-Updates/index.js
+++ Lesson6-Element-Updates/index.js
@@ -1,1 +1,30 @@
+let counter = 0;
 
+for(let i=0;i<10;i++){
+    counter++
+    const $span = $(`<span>Hell: ${counter}</span>`);     
+    $span.text('New')
+    $('.div2').after($span);
+}
+
+for(let i=0;i<3;i++){
+   $(`<button>Hell: ${i+1}</button>`).
+   insertAfter('.div3').click(function(){
+    console.log($(this))
+   })     
+
+}
+
+
+$('#btn1').click(()=>{
+    const val = $('#input').val()
+    $('.div1').text(val);
+    $('.div3').prepend(val);
+})
+
+
+$('#btn2').click(()=>{
+    counter++;
+    const $div = $(`<div>Hell: ${counter}</div>`);     
+    $div.appendTo('.div3');
+})
