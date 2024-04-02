45af28e5fb431090c0a3037b7db89798dabb9a6b
Berk Akın <56769507+BerkAkin@users.noreply.github.com>
2024-04-03 00:28:26 +0300

    practised get data using jquery

--- Lesson26-Get-JSON-Data/index.js
+++ Lesson26-Get-JSON-Data/index.js
@@ -7,15 +7,17 @@ $('#btn3').click(adder3);
 function adder1(){
     $.get('file30.json', (res)=>{
         console.log(res);
+
         let html = `${res.firstName} ${res.lastName}`;
         $('<h2>').text(html).appendTo('.output');
+
         const a = res.address;
         const val1 = `${a.city} <br> ${a.postalCode} <br> ${a.state} <br> ${a.streetAddress} <br> `
         $('<div>').html(val1).appendTo('.output');
+
         $ul = $('<ul>');
         $ul.appendTo('.output');
         $.each(res.phoneNumbers, (index, value)=>{
-            console.log(value);
             const temp = `${index+1}. ${value.number} (${value.type})`;
             $('<li>').html(temp).appendTo($ul);
         })
@@ -26,7 +28,7 @@ function adder1(){
 function adder2(){
     $.get('https://discoveryvip.com/shared/people.json', (data)=>{
         console.log(data);
-        $.each(data.people, (index,value)=>{console.log(value.first)})
+        $.each(data.people, (index, value)=>{console.log(value.first)})
     })
 }
 
@@ -34,7 +36,7 @@ function adder3(){
     $.get('https://www.discoveryvip.com/shared/books1.json', (data)=>{
         console.log(data.books);
         let val1 = JSON.stringify(data.books);
-        $.each(data.books,(ind,val)=>{
+        $.each(data.books,(ind, val)=>{
             val1 += `<div>(${val.title})</div>`
             val1 += `<div>(${val.author})</div>`
         })
